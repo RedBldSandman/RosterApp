b7b8f9ece4bcb8002f9775a7f0e5722bdede1cb3
Ahrandeep Aujla <ahran@aujla.org>
2015-07-30 01:23:53 +0100

    page title updates

---
+++ RosterApp/public/js/controllers.js
@@ -0,0 +1,5 @@
+﻿angular.module('rosterApp.controllers', []).
+	controller('homeController', function ($scope) {
+		//Load product or use resolve in routing
+		//$scope.page.setTitle($scope.title);
+});
